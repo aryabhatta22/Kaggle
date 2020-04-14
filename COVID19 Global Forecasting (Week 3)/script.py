68b7d1f416f38cd4294ab04ac6b3464a7492a72e
aryabhatta <tarun.hive@hotmail.com>
2020-04-14 22:58:53 +0530

    model prepared with no issue

--- COVID19 Global Forecasting (Week 3)/script.py
+++ COVID19 Global Forecasting (Week 3)/script.py
@@ -1,18 +1,29 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 df_train = pd.read_csv('train.csv')
+df_train['Province_State'].fillna("",inplace = True)        # for avoiding error during conctination
 df_test = pd.read_csv('test.csv')
+df_test['Province_State'].fillna("",inplace = True)
 df_sub = pd.read_csv('submission.csv')
                 
                                     # Data Preprocessing
                 
-X = df_train.iloc[:, [2,3]].values
+X = df_train.iloc[:, [1,2,3]].values
 y = df_train.iloc[:, [4,5]].values
+X_test = df_test.iloc[:, [1,2,3]].values
+X_sub = df_sub.iloc[:,0:4].values
 
-#print(df_train.isnull().any())
-#print(df_test.isnull().any())
+X[:,1] = X[:,0]+" "+X[:,1]      # to concatinate province and country so that different model could be created for different region
+X = X[:, [1,2]]                 # to consider country(province + country) & date only
+X_test[:,1] = X_test[:,0]+" "+X_test[:,1]
+X_test = X_test[:,[1,2]]
+
+lengthX = len(X)
+X = np.append(X, X_test, axis= 0)       # So that label encoder could match in both set
+
 
 from sklearn.preprocessing import LabelEncoder
 labelencoder_X_1 = LabelEncoder()
@@ -20,25 +31,46 @@ X[:, 0] = labelencoder_X_1.fit_transform(X[:, 0])
 labelencoder_X_2 = LabelEncoder()
 X[:, 1] = labelencoder_X_2.fit_transform(X[:,1])
 
-#temp = X.astype(int)
-#temp = temp[y[:,0].sort()]  
+X_test[:, 0] = labelencoder_X_1.transform(X_test[:, 0]) 
+X_test[:, 1] = labelencoder_X_2.transform(X_test[:,1])
 
-                                    # CONFIRMED CASE PREDICTION
+X= X[0:lengthX,:]                   # getting original training set
 
-def ConfirmedCasePrediction(countryCode,startingIndex):
+
+                                    # UPDATE SUBMISSION file
+                                    
+def updateSubmission(regressor, countryCode,label):
+    if label == "ConfirmedCases":
+        j = 1
+    elif label == "Fatalities":
+        j =2
+    X_country = X_test[X_test[:,0] == countryCode] 
+    for i in range(0, len(X_country)):
+        X_sub[i,j] = math.floor(regressor.predict(np.asarray(X_country[i,1]).reshape(1,1)))
+
+                                    # CONFIRMED CASE/ FATALITIES PREDICTION
+
+def ConfirmedCasePrediction(countryCode,startingIndex, label):
+    if label == "ConfirmedCases":
+        j = 0
+    elif label == "Fatalities":
+        j =1
+    
     i=startingIndex
     X_country = X[X[:,0] == countryCode]                          # X for particular country
-    y_country = y[i:i+len(X_country)]                   # y for selected country 
+    y_country = y[i:i+len(X_country)]                               # y for selected country 
     
     print("for country "+ str((labelencoder_X_1.inverse_transform([countryCode]))[0]) +" min case "+str (min(y_country[:,0]))+ " max case " +str( max(y_country[:,0])))
-    print(" >> start index "+ str(i))
-    print(" >> end index "+ str(i+len(X_country)))
+
     
                                         # Random Forest                                     
     from sklearn.ensemble import RandomForestRegressor
     regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
-    regressor.fit(X_country[:,1].reshape(len(X_country), 1),y_country[:,0]) 
+    regressor.fit(X_country[:,1].reshape(len(X_country), 1),y_country[:,j]) 
+    updateSubmission(regressor, countryCode,label)
                         # visulaizing data 
+    """
+    # Avoid if you dont you just want output not graphs
     X_grid = np.arange(min(X_country[:,1]), max(X_country[:,1]), 0.01)
     X_grid = X_grid.reshape((len(X_grid)), 1) 
     plt.ion()
@@ -50,12 +82,25 @@ def ConfirmedCasePrediction(countryCode,startingIndex):
     plt.xlabel("Day")
     plt.ylabel('Confirmed Cases')
     plt.show() 
-                        # preydictions
-    #import math
-    #y_pred = regressor.predict(X_country[:,1].reshape(len(X_country),1))
-    #y_pred = [math.floor(case) for case in y_pred]
-
+    """
+    
+# for predicting Fatalities
 startIndex =0
 for i in range(0,len(np.unique(X[:,0]))):
-    ConfirmedCasePrediction(i,startIndex)
+    ConfirmedCasePrediction(i,startIndex,"Fatalities")
     startIndex = startIndex+ len(X[X[:,0] == i])
+
+# for predicting confirmed cases
+startIndex = 0
+for i in range(0,len(np.unique(X[:,0]))):
+    ConfirmedCasePrediction(i,startIndex,"ConfirmedCases")
+    startIndex = startIndex+ len(X[X[:,0] == i])
+    
+"""
+  
+# For kaggle submission only
+  
+my_submission = pd.DataFrame({'ForecastId': X_sub[:,0], 'ConfirmedCases': X_sub[:,1], 'Fatalities': X_sub[:,2]})
+my_submission.to_csv('submission.csv', index=False)
+
+"""
