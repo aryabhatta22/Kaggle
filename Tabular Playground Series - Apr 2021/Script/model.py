33c55597c6a1e43714d9346649ab41c0c7753985
aryabhatta <tarun.hive@hotmail.com>
2021-04-17 23:55:42 +0530

    tested on XGBoost, model performed best

--- Tabular Playground Series - Apr 2021/Script/model.py
+++ Tabular Playground Series - Apr 2021/Script/model.py
@@ -7,6 +7,7 @@ Created on Sat Apr  3 23:27:30 2021
 
 import numpy as np
 import pandas as pd
+import time
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
@@ -64,7 +65,7 @@ sub_X, _ = preProcessData(df_test, False)
 
 
 # Train Test Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0, stratify = y)
 
 
 """
@@ -77,6 +78,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from xgboost import XGBClassifier
+
 
 # K nearest neighbors
 clf_knn = KNeighborsClassifier(n_neighbors = 5, metric ='minkowski', p =2)
@@ -88,7 +91,7 @@ ac_knn = accuracy_score(y_test,y_pred_knn)
 
 
 # random forest
-clf_rf = RandomForestClassifier(max_depth=10, random_state=0)
+clf_rf = RandomForestClassifier(n_estimators=100,max_depth=10, random_state=0)
 clf_rf.fit(X_train,y_train)
 
 y_pred_rf = clf_rf.predict(X_test)
@@ -162,6 +165,51 @@ y_pred_ann = np.where(y_pred_ann >= 0.5, 1,0)
 cm_ann = confusion_matrix(y_test, y_pred_ann)
 ac_ann = accuracy_score(y_test,y_pred_ann)
 
+# XGBoost Classifier
+clf_xgb= XGBClassifier(n_estimators = 150, max_depth  = 8,learning_rate = 0.001,
+                       verbosity =1,subsample=0.6,min_child_weight=10,gamma =5,
+                       colsample_bytree=1.0)
+clf_xgb.fit(X_train, y_train)
+
+y_pred_xgb = clf_xgb.predict(X_test)
+cm_xgb = confusion_matrix(y_test, y_pred_xgb)
+ac_xgb = accuracy_score(y_test,y_pred_xgb)
+
+#GridSeach
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import StratifiedKFold
+
+params = {
+        'n_estimators': [10,25,50,100,150,175,200,225],
+        'max_depth': [5,8,10,20],
+        'min_child_weight': [1, 5, 10,20,30],
+        'gamma': [0.5, 1, 1.5, 2, 5,7.5,8,10],
+        'subsample': [0.2,0.3,0.6],
+        'colsample_bytree': [0.6, 0.8, 1.0,1.2,1.5],
+        }
+
+clf_xgb= XGBClassifier( learning_rate = 0.001,
+                       verbosity =1 )
+folds = 5
+param_comb = 6
+
+skf = StratifiedKFold(n_splits=folds, shuffle = True, random_state = 0)
+random_search = RandomizedSearchCV(clf_xgb, param_distributions=params,
+                                   n_iter=param_comb, scoring='accuracy',
+                                   n_jobs=-1, cv=skf.split(X,y), verbose=3, random_state=0 )
+
+print('Random Search Started  ...........\n\n')
+start = time.process_time()
+random_search.fit(X, y)
+print('Random Search Finsihed!!! Took '+str(((time.process_time() - start)/60))+" minutes\n\n")
+
+print('\n Best estimator:')
+print(random_search.best_estimator_)
+print('\n Best normalized gini score for %d-fold search with %d parameter combinations:' % (folds, param_comb))
+print(random_search.best_score_ * 2 - 1)
+print('\n Best hyperparameters:')
+print(random_search.best_params_)
+
 """
  Print Performance
 """
@@ -183,11 +231,11 @@ table_.add_row(["ANN","{:.2f}".format(ac_ann)])
 
 df_sub  = pd.read_csv('../Data/sample_submission.csv')
 pessenger_id = df_sub.loc[:,'PassengerId'].values
-survive_pred = clf_ann.predict(sub_X)
+survive_pred = clf_xgb.predict(sub_X)
 survive_pred = np.where(survive_pred >= 0.5, 1,0)
 
 Submission_mat = np.concatenate((pessenger_id.reshape(len(pessenger_id),1),
                                  survive_pred.reshape(len(survive_pred),1)), axis = 1)
 df_submission = pd.DataFrame(Submission_mat, columns=df_sub.columns.values)
 
-df_submission.to_csv("../Data/submission_ANN_N5N3N1.csv", index=False)
+df_submission.to_csv("../Data/submission_XGB.csv", index=False)
